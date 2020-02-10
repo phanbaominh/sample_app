fd5eb78fa54c79ac6e814e1422f1247cf2a6b65f
Minh Phan <pbminh2511@gmail.com>
2020-02-10 22:06:46 +0700

    Add title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+    def full_title(base_title = '')
+        base_title.empty? ? "Ruby on Rails Tutorial Sample App"
+            : base_title + ' | ' + "Ruby on Rails Tutorial Sample App"
+    end
 end
