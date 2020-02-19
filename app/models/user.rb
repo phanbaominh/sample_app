68e358fea9c968db7e64e3e0e372f9479ac2d694
Minh Phan <pbminh2511@gmail.com>
2020-02-19 11:02:01 +0700

    Temp commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,11 @@ class User < ApplicationRecord
     validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
     has_many :microposts, dependent: :destroy
+    has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+    has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+    has_many :following, through: :active_relationships, source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
+    
     def User.digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                       BCrypt::Engine.cost
@@ -63,6 +68,20 @@ class User < ApplicationRecord
         Micropost.where("user_id = ?", id)
     end
 
+    def follow(other_user)
+        following << other_user
+    end
+
+    # Unfollows a user.
+    def unfollow(other_user)
+        following.delete(other_user)
+    end
+
+    # Returns true if the current user is following the other user.
+    def following?(other_user)
+        following.include?(other_user)
+    end
+    
     private
         def downcase_email
             self.email.downcase!
